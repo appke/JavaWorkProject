58231965deca6d2c8e483656541ef08462345190
mul001 <mul001@qianhai.com.cn>
2019-05-14 15:58:10 +0800

    Add: 第一个测试

--- day02-code/src/com/appke/Test.java
+++ day02-code/src/com/appke/Test.java
@@ -2,6 +2,15 @@ package com.appke;
 
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("人生的际遇啊…");
+
+
+        int age;
+        String name;
+
+
+
     }
 }
