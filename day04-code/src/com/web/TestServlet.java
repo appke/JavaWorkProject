eef00b293eeb5b857bca63ba7a8dfa4a2c1e2a5c
mul001 <mul001@qianhai.com.cn>
2019-05-21 08:59:32 +0800

    添加web项目

---
+++ day04-code/src/com/web/TestServlet.java
@@ -0,0 +1,36 @@
+package com.web;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+
+public class TestServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+        // 提供服务的方法
+        System.out.println("Hello Servlet……");
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- day04-code/web/index.jsp
+++ day04-code/web/index.jsp
@@ -1,18 +1,19 @@
 <%--
   Created by IntelliJ IDEA.
   User: mgbook
-  Date: 2019-05-20
-  Time: 13:25
+  Date: 2019-05-21
+  Time: 08:34
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title> Hello, Web Application </title>
+    <title>$Title$</title>
   </head>
   <body>
 
-    Hello, Web Application
+    Hello Servlet ------
+
 
   </body>
 </html>
